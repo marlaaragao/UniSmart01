ac686f4eeb6d69dca6e2cfbb8cf28a74bf1afb41
Marla Aragao <vargas.marla90@gmail.com>
2015-06-04 13:38:47 -0300

    Atualização Bibliotecas
    Atualização para Vaadin 7.4.6

---
+++ src/java/views/alunos/LayoutHistorico.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package views.alunos;
+
+/**
+ * Classe de Layout que mostra o histórico do Aluno no Semestre atual, 
+ * ou escolhendo outro Período.
+ * 
+ * Relacionada ao cumprimento do requisito R1 - Permitir Visualização 
+ * de Notas Obtidas no Semestre Letivo.
+ * 
+ * @author Marla Aragão
+ */
+public class LayoutHistorico {
+    
+}
